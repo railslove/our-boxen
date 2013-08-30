156bd970b9fbbc74f4af35751924bd0bbf06a401
Michael Bumann <michael@railslove.com>
2013-08-30 16:30:55 +0200

    debug phantomjs global for now

--- manifests/site.pp
+++ manifests/site.pp
@@ -67,7 +67,6 @@ node default {
   include postgresql
   include elasticsearch
   include phantomjs::1_9_0
-  include phantomjs::global { '1.9.0': }
   include mongodb
   include redis
   include zsh
