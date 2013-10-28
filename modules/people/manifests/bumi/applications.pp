1ea3216e9da7f9606eede12db136890fccadece6
Michael Bumann <michael@railslove.com>
2013-10-28 19:18:43 +0300

    remove applications that should be installed via the appstore

--- modules/people/manifests/bumi/applications.pp
+++ modules/people/manifests/bumi/applications.pp
@@ -1,7 +1,6 @@
 class people::bumi::applications {
   include chrome
   include arq
-  include caffeine
   include colloquy
   include viscosity
   include transmission
@@ -13,8 +12,6 @@ class people::bumi::applications {
   include alfred
   include iterm2::stable
   include skype 
-  include mumble
-  include evernote
   include screenhero
   include sequel_pro
   include virtualbox
