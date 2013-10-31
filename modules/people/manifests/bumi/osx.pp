0dbcc928c159dba768a29da2d6061368087fc364
Michael Bumann <michael@railslove.com>
2013-10-31 15:19:48 +0300

    tmp remove problematic osx settings

--- modules/people/manifests/bumi/osx.pp
+++ modules/people/manifests/bumi/osx.pp
@@ -1,15 +1,12 @@
 class people::bumi::osx {
   
-  osx::recovery_message { 'If this Mac is found, please call +49 176 22747893': }
   include osx::global::enable_keyboard_control_access
   include osx::global::expand_print_dialog
   include osx::global::expand_save_dialog
   include osx::global::disable_autocorrect
-  include osx::global::key_repeat_delay
   class { 'osx::global::key_repeat_delay':
     delay => 25
   }
-  include osx::global::key_repeat_rate
   class { 'osx::global::key_repeat_rate':
     rate => 2
   }
@@ -17,7 +14,6 @@ class people::bumi::osx {
   include osx::universal_access::ctrl_mod_zoom
   include osx::no_network_dsstores
   include osx::dock::clear_dock
-  include osx::dock::icon_size
   class { 'osx::dock::icon_size': 
     size => 20
   }
@@ -27,8 +23,7 @@ class people::bumi::osx {
     user   => $::boxen_user,
     domain => 'NSGlobalDomain',
     key    => 'AppleShowAllExtensions',
-    value  => true, 
-    notify => Exec['killall Finder'];
+    value  => true
   }
 
   boxen::osx_defaults { 'Disable the warning when changing a file extension':
@@ -36,8 +31,7 @@ class people::bumi::osx {
     user   => $::boxen_user,
     domain => 'com.apple.finder',
     key    => 'FXEnableExtensionChangeWarning',
-    value  => false,
-    notify => Exec['killall Finder'];
+    value  => false
   }
 
 
@@ -45,8 +39,7 @@ class people::bumi::osx {
     user   => $::boxen_user,
     domain => 'com.apple.driver.AppleBluetoothMultitouch.trackpad',
     key    => 'Clicking',
-    value  => true,
-    notify => Exec['killall Finder'];
+    value  => true
   }
   boxen::osx_defaults { 'tap to click for trackpads':
     ensure => present,
@@ -54,8 +47,7 @@ class people::bumi::osx {
     domain => 'NSGlobalDomain',
     key    => 'com.apple.mouse.tapBehavior',
     value  => 1,
-    type   => "int",
-    notify => Exec['killall Finder'];
+    type   => "int"
   }
 }
 
